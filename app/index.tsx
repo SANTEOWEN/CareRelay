ed3515e04d34f0c3a5d70c6522f39135451c4cd9
OWENSANTE <owensante45@gmail.com>
2025-09-09 23:54:05 +0800

    Learning Routers and navigation from Expo and added some small changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,14 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 import "./global.css";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-blue-700">
-      <Text>TEST</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-slate-800 font-bold">WELCOME!</Text>
+      <Link href="/onboarding" >
+        Onboarding
+      </Link>
     </View>
   );
 }
