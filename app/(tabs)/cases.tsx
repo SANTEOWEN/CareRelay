14dec21ae2d55e9be2d622a0c6b747e078a43609
OWENSANTE <owensante45@gmail.com>
2025-09-19 10:30:44 +0800

    This are the 3 new screens for the app.

---
+++ app/(tabs)/cases.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function cases() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold">CASES</Text>
+    </View>
+  );
+}
+
---
+++ app/(tabs)/history.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function history() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold">HISTORY</Text>
+    </View>
+  );
+}
+
