db99dc60dc4520523685819e35c00d00bd95fb88
OWENSANTE <owensante45@gmail.com>
2025-09-19 10:30:30 +0800

    layout used for tabs screen. This includes all of the tabs used on the screens.

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,25 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Tabs } from 'expo-router';
+import React from "react";
+
+export default function TabsLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen name="dashboard" options={{ 
+        title: "Dashboard",
+        tabBarIcon:({color}) => <MaterialIcons name="dashboard" size={24} color={color} />
+      }} />
+      <Tabs.Screen name="cases" options={{ 
+        title: "Cases",
+        tabBarIcon:({color}) => <MaterialCommunityIcons name="folder-heart-outline" size={24} color="black" />
+      }} />
+      <Tabs.Screen name="history" options={{ 
+        title: "History",
+        tabBarIcon:({color}) => <FontAwesome name="history" size={24} color={color} />
+      }} />
+    </Tabs>
+  );
+}
+
