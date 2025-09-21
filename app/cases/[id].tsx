962e740ed4d79ea0e2e1300ceee0d141e0b8675f
OWENSANTE <owensante45@gmail.com>
2025-09-21 21:15:57 +0800

    added [ID] for dynamic viewing of cases in the future. this is for the file structure of the data.

---
+++ app/cases/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const CasesDetails = () => {
+  return (
+    <View>
+      <Text>CasesDetails</Text>
+    </View>
+  )
+}
+
+export default CasesDetails
