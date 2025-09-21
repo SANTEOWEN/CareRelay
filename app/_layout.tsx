cf1543b0f79f578e30aa13811a7c3199d655e70b
OWENSANTE <owensante45@gmail.com>
2025-09-21 21:13:49 +0800

    Included Portalhost for the DIfferent primitives of ReactNativeReusables and other primitives in the future.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,14 @@
+import { PortalHost } from '@rn-primitives/portal';
 import { Stack } from "expo-router";
 import React from "react";
 import "./global.css";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
+        <PortalHost />
+      </Stack>
   );
 }
 
