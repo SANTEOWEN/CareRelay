6932aafc2e4167622ca0909304f6a42633af4bb8
OWENSANTE <owensante45@gmail.com>
2025-09-21 21:14:41 +0800

    Added SafeAreaProvider and for the tabs and adjusted/testings new learned attributes for the options in Tabs component.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,23 +3,28 @@ import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { Tabs } from 'expo-router';
 import React from "react";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="dashboard" options={{ 
-        title: "Dashboard",
-        tabBarIcon:({color}) => <MaterialIcons name="dashboard" size={24} color={color} />
-      }} />
-      <Tabs.Screen name="cases" options={{ 
-        title: "Cases",
-        tabBarIcon:({color}) => <MaterialCommunityIcons name="folder-heart-outline" size={24} color="black" />
-      }} />
-      <Tabs.Screen name="history" options={{ 
-        title: "History",
-        tabBarIcon:({color}) => <FontAwesome name="history" size={24} color={color} />
-      }} />
-    </Tabs>
+    <SafeAreaProvider>
+      <Tabs>
+        <Tabs.Screen name="dashboard" options={{ 
+          title: "Dashboard",
+          tabBarIcon:({color}) => <MaterialIcons name="dashboard" size={24} color={color} />,
+          headerTitleAlign: "center",
+          tabBarActiveTintColor: "#154e63"
+        }} />
+        <Tabs.Screen name="cases" options={{ 
+          title: "Cases",
+          tabBarIcon:({color}) => <MaterialCommunityIcons name="folder-heart-outline" size={24} color={color} />
+        }} />
+        <Tabs.Screen name="history" options={{ 
+          title: "History",
+          tabBarIcon:({color}) => <FontAwesome name="history" size={24} color={color} />
+        }} />
+      </Tabs>
+    </SafeAreaProvider>
   );
 }
 
